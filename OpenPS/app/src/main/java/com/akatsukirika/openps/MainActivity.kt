2f452611b55e56bb3c376a913ad37cba15dc079e
AkatsukiRika <m1345860061@gmail.com>
2024-07-24 11:05:06 +0800

    add: Android主项目

---
+++ OpenPS/app/src/main/java/com/akatsukirika/openps/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.akatsukirika.openps
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import com.akatsukirika.openps.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Example of a call to a native method
+        binding.sampleText.text = stringFromJNI()
+    }
+
+    /**
+     * A native method that is implemented by the 'openps' native library,
+     * which is packaged with this application.
+     */
+    external fun stringFromJNI(): String
+
+    companion object {
+        // Used to load the 'openps' library on application startup.
+        init {
+            System.loadLibrary("openps")
+        }
+    }
+}
