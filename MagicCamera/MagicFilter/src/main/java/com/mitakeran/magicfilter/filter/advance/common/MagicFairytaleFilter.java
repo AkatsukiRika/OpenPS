f1966cd1e8dbdeee58f51c8c79de7ed3fa872fa9
AkatsukiRika <m1345860061@gmail.com>
2024-10-24 13:46:31 +0800

    feat: 整理代码，使MagicCamera项目通过编译

---
+++ MagicCamera/MagicFilter/src/main/java/com/mitakeran/magicfilter/filter/advance/common/MagicFairytaleFilter.java
@@ -0,0 +1,12 @@
+package com.mitakeran.magicfilter.filter.advance.common;
+
+import android.content.Context;
+
+import com.mitakeran.magicfilter.filter.base.MagicLookupFilter;
+
+public class MagicFairytaleFilter extends MagicLookupFilter{
+
+	public MagicFairytaleFilter(Context context) {
+		super(context, "filter/fairy_tale.png");
+	}
+}
