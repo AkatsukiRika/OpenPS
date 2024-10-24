7eabd2730c925b1befb0760b965b4b74c3f17b18
AkatsukiRika <m1345860061@gmail.com>
2024-10-24 09:46:46 +0800

    feat: 删除ShaderTest仓库

--- ShaderTest/app/src/main/assets/vertex_shader.glsl
+++
@@ -1,9 +0,0 @@
-attribute vec4 vPosition;
-attribute vec2 aTexCoord;
-varying vec2 vTexCoord;
-uniform mat4 uMVPMatrix;
-
-void main() {
-    gl_Position = uMVPMatrix * vPosition;
-    vTexCoord = aTexCoord;
-}
