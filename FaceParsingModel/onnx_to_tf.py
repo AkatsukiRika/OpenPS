7d42aa1ff73096c854257bd9c54b975c5b2923ad
AkatsukiRika <m1345860061@gmail.com>
2024-12-26 10:07:59 +0800

    feat: MI-GAN模型

--- FaceParsingModel/onnx_to_tf.py
+++ FaceParsingModel/onnx_to_tf.py
@@ -1,6 +1,6 @@
 from onnx_tf.backend import prepare
 import onnx
 
-onnx_model = onnx.load('output/79999_iter.onnx')
+onnx_model = onnx.load('migan_pipeline_v2.onnx')
 tf_rep = prepare(onnx_model)
-tf_rep.export_graph('output/79999_iter.pb')
+tf_rep.export_graph('output/migan_pipeline_v2.pb')
